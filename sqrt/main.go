68523ded8625a5091784f6320bec0eb1c2abcf30
Benjamin-Couey <benjamincouey36@gmail.com>
2024-05-31 08:58:14 -0600

    Added test and implementation for sqrt(x)

---
+++ sqrt/main.go
@@ -0,0 +1,11 @@
+package sqrt
+
+/* Assumes that:
+0 <= x <= 2^31 - 1 */
+func MySqrt(x int) int {
+	guess := float32(1)
+	for x != int( guess * guess ) {
+		guess = ( guess + float32(x) / guess ) / float32(2)
+	}
+	return int(guess)
+}
---
+++ sqrt/sqrt_test.go
@@ -0,0 +1,30 @@
+package sqrt
+
+import (
+	"testing"
+)
+
+func TestMySqrt(t *testing.T) {
+	testcases := []struct {
+		x, solution int
+	}{
+		{ 4, 2 },
+		{ 8, 2 },
+		{ 16, 4 },
+		{ 17, 4 },
+		{ 1, 1 },
+		{ 0, 0 },
+	}
+
+	for _, testcase := range testcases {
+		result := MySqrt( testcase.x )
+		if result != testcase.solution {
+			t.Errorf(
+				"MySqrt: %v returned %v, want %v",
+				testcase.x,
+				result,
+				testcase.solution,
+			)
+		}
+	}
+}
