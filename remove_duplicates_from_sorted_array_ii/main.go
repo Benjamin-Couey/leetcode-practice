5ae9e39c4bba8593ac287c98b5c7e932483b494d
Benjamin-Couey <benjamincouey36@gmail.com>
2024-06-04 07:48:16 -0600

    Added test and implementation for remove_duplicates_from_sorted_array_ii

---
+++ remove_duplicates_from_sorted_array_ii/main.go
@@ -0,0 +1,28 @@
+package remove_duplicates_from_sorted_array_ii
+
+/*Assumes that:
+1 <= nums.length <= 3 * 10^4
+-10^4 <= nums[i] <= 10^4
+nums is sorted in non-decreasing order. */
+func RemoveDuplicates(nums []int) int {
+	count := 1
+	current_num := nums[0]
+	index := 1
+	upper_bound := len( nums )
+	for index < upper_bound {
+		if nums[index] == current_num {
+			count++
+			if count > 2 {
+				nums = append( nums[:index], nums[index+1:]...)
+				upper_bound--
+			} else {
+				index++
+			}
+		} else {
+			count = 1
+			current_num = nums[index]
+			index++
+		}
+	}
+	return index
+}
---
+++ remove_duplicates_from_sorted_array_ii/remove_duplicates_from_sorted_array_ii_test.go
@@ -0,0 +1,30 @@
+package remove_duplicates_from_sorted_array_ii
+
+import (
+	"testing"
+	"leetcode/utils"
+)
+
+func TestRemoveDuplicates(t *testing.T) {
+	testcases := []struct {
+		nums, solution []int
+	}{
+		{ []int{ 1, 1, 1, 2, 2, 3 }, []int{ 1, 1, 2, 2, 3 }, },
+		{ []int{ 0, 0, 1, 1, 1, 1, 2, 3, 3 }, []int{ 0, 0, 1, 1, 2, 3, 3 }, },
+		{ []int{ 0, 0, 1, 1, 1, 1, 2, 3, 3, 4 }, []int{ 0, 0, 1, 1, 2, 3, 3, 4 }, },
+	}
+
+	for _, testcase := range testcases {
+		k := RemoveDuplicates( testcase.nums )
+
+		if k != len( testcase.solution ) {
+			t.Errorf("RemoveDuplicates: returned %v, want %v", testcase.nums, testcase.solution)
+		}
+
+		first_k := testcase.nums[0:k]
+		if !utils.SliceEqual( first_k, testcase.solution ) {
+			t.Errorf("RemoveDuplicates: returned %v, want %v", first_k, testcase.solution)
+		}
+
+	}
+}
