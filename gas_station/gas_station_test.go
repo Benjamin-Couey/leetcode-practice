c8697ffa05b1cbe0e7266d09052bc3a77f0082ff
Benjamin-Couey <benjamincouey36@gmail.com>
2024-06-10 08:30:50 -0600

    Added tests and implementation for gas_Station

---
+++ gas_station/gas_station_test.go
@@ -0,0 +1,33 @@
+package gas_station
+
+import (
+	"testing"
+)
+
+func TestCanCompleteCircuit(t *testing.T) {
+	testcases := []struct {
+		gas, cost []int
+		solution int
+	}{
+		{ []int{ 1, 2, 3, 4, 5 }, []int{ 3, 4, 5, 1, 2 }, 3 },
+		{ []int{ 1, 2, 3, 4, 5 }, []int{ 3, 2, 5, 2, 3 }, 3 },
+		{ []int{ 1, 2, 3, 4, 5 }, []int{ 3, 1, 5, 3, 3 }, 3 },
+		{ []int{ 1, 2, 3, 4, 5 }, []int{ 3, 1, 2, 2, 7 }, 1 },
+		{ []int{ 3, 2, 4, 4, 1 }, []int{ 1, 3, 1, 1, 8 }, 0 },
+		{ []int{ 2, 3, 4 }, []int{ 3, 4, 3 }, -1 },
+	}
+
+	for _, testcase := range testcases {
+		result := CanCompleteCircuit( testcase.gas, testcase.cost )
+
+		if result != testcase.solution {
+			t.Errorf(
+				"CanCompleteCircuit: %v and %v returned %v, want %v",
+				testcase.gas,
+				testcase.cost,
+				result,
+				testcase.solution,
+			)
+		}
+	}
+}
---
+++ gas_station/main.go
@@ -0,0 +1,36 @@
+package gas_station
+
+/* Assumes that:
+If there exists a solution, it is guaranteed to be unique.
+n == gas.length == cost.length
+1 <= n <= 10^5
+0 <= gas[i], cost[i] <= 10^4 */
+func CanCompleteCircuit(gas []int, cost []int) int {
+
+	net_sum := 0
+	net_gas := make( []int, len(gas) )
+
+	for index, gas_num := range gas {
+		net_sum += gas_num - cost[ index ]
+		net_gas[ index ] = gas_num - cost[ index ]
+	}
+
+	if net_sum < 0 {
+		return net_sum
+	}
+
+	left_sum := 0
+	return_sum := left_sum
+	return_index := 0
+	for index, _ := range net_gas {
+		if index > 0 {
+			left_sum += net_gas[ index - 1 ]
+			if left_sum < return_sum {
+				return_sum = left_sum
+				return_index = index
+			}
+		}
+	}
+
+	return return_index
+}
