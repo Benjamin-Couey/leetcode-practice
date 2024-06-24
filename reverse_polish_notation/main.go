a3f7955ea37965bd1c744bfa7132f84b651f1435
Benjamin-Couey <benjamincouey36@gmail.com>
2024-06-24 08:45:37 -0600

    Added tests and implementation for reverse_polish_notation

---
+++ reverse_polish_notation/main.go
@@ -0,0 +1,61 @@
+package reverse_polish_notation
+
+import (
+	"fmt"
+)
+
+const ADD string = "+"
+const SUBTRACT string = "-"
+const MULTIPLY string = "*"
+const DIVIDE string = "/"
+
+/* Assumes that:
+Each operand may be an integer or another expression.
+The division between two integers always truncates toward zero.
+There will not be any division by zero.
+The input represents a valid arithmetic expression in a reverse polish notation.
+The answer and all the intermediate calculations can be represented in a 32-bit integer.
+1 <= tokens.length <= 10^4
+tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200]. */
+func EvalRPN(tokens []string) int {
+
+	stack := make( []string, 0 )
+
+	for _, token := range tokens {
+		if len( stack ) > 1 {
+			last_index := len( stack ) - 1
+			var right_operand int
+			var left_operand int
+			var result int
+			performed_operation := false
+			_, _ = fmt.Sscanf( stack[ last_index ], "%d", &right_operand )
+			_, _ = fmt.Sscanf( stack[ last_index - 1 ], "%d", &left_operand )
+			switch token {
+				case ADD:
+					result = left_operand + right_operand
+					performed_operation = true
+				case SUBTRACT:
+					result = left_operand - right_operand
+					performed_operation = true
+				case MULTIPLY:
+					result = left_operand * right_operand
+					performed_operation = true
+				case DIVIDE:
+					result = left_operand / right_operand
+					performed_operation = true
+				default:
+					stack = append( stack, token )
+			}
+			if performed_operation {
+				stack = stack[:last_index - 1]
+				stack = append( stack, fmt.Sprint( result ) )
+			}
+		} else {
+			stack = append( stack, token )
+		}
+	}
+	last_index := len( stack ) - 1
+	var result int
+	_, _ = fmt.Sscanf( stack[ last_index ], "%d", &result )
+  return result
+}
---
+++ reverse_polish_notation/reverse_polish_notation_test.go
@@ -0,0 +1,30 @@
+package reverse_polish_notation
+
+import (
+	"testing"
+)
+
+func TestEvalRPN(t *testing.T) {
+	testcases := []struct {
+		tokens []string
+		solution int
+	}{
+		{ []string{ "2", "1", "+", "3", "*" }, 9 },
+		{ []string{ "4", "13", "5", "/", "+" }, 6 },
+		{ []string{ "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+" }, 22 },
+		{ []string{ "-1" }, -1 },
+	}
+
+	for _, testcase := range testcases {
+		result := EvalRPN( testcase.tokens )
+
+		if result != testcase.solution {
+			t.Errorf(
+				"EvalRPN: %v returned %v, want %v",
+				testcase.tokens,
+				result,
+				testcase.solution,
+			)
+		}
+	}
+}
