1b9b8a2e5ff3dac9030c1f4410a6b0e2f74f117d
Benjamin-Couey <benjamincouey36@gmail.com>
2024-06-27 09:27:15 -0600

    Added tests and implementation for surrounded_regions

---
+++ surrounded_regions/surrounded_regions_test.go
@@ -0,0 +1,69 @@
+package surrounded_regions
+
+import (
+	"testing"
+	"leetcode/utils"
+)
+
+func TestSolve(t *testing.T) {
+	testcases := []struct {
+		matrix [][]byte
+		solution [][]byte
+	}{
+		{ [][]byte{
+				{ byte('X'), byte('X'), byte('X'), byte('X') },
+				{ byte('X'), byte('O'), byte('O'), byte('X') },
+				{ byte('X'), byte('X'), byte('O'), byte('X') },
+				{ byte('X'), byte('O'), byte('X'), byte('X') },
+			}, [][]byte{
+				{ byte('X'), byte('X'), byte('X'), byte('X') },
+				{ byte('X'), byte('X'), byte('X'), byte('X') },
+				{ byte('X'), byte('X'), byte('X'), byte('X') },
+				{ byte('X'), byte('O'), byte('X'), byte('X') },
+			},
+		},
+		{ [][]byte{
+				{ byte('X'), byte('X'), byte('X'), byte('X') },
+				{ byte('X'), byte('O'), byte('O'), byte('O') },
+				{ byte('X'), byte('O'), byte('X'), byte('X') },
+				{ byte('X'), byte('X'), byte('X'), byte('X') },
+			}, [][]byte{
+				{ byte('X'), byte('X'), byte('X'), byte('X') },
+				{ byte('X'), byte('O'), byte('O'), byte('O') },
+				{ byte('X'), byte('O'), byte('X'), byte('X') },
+				{ byte('X'), byte('X'), byte('X'), byte('X') },
+			},
+		},
+		{ [][]byte{
+				{ byte('O'), byte('X'), byte('O'), byte('X') },
+				{ byte('X'), byte('O'), byte('X'), byte('O') },
+				{ byte('O'), byte('X'), byte('O'), byte('X') },
+				{ byte('X'), byte('O'), byte('X'), byte('O') },
+			}, [][]byte{
+				{ byte('O'), byte('X'), byte('O'), byte('X') },
+				{ byte('X'), byte('X'), byte('X'), byte('O') },
+				{ byte('O'), byte('X'), byte('X'), byte('X') },
+				{ byte('X'), byte('O'), byte('X'), byte('O') },
+			},
+		},
+		{ [][]byte{
+				{ byte('X'), byte('O'), byte('X'), byte('X') },
+			}, [][]byte{
+				{ byte('X'), byte('O'), byte('X'), byte('X') },
+			},
+		},
+		{ [][]byte{ { byte('O') } }, [][]byte{ { byte('O') } } },
+	}
+
+	for _, testcase := range testcases {
+		Solve( testcase.matrix )
+
+		if !utils.MatrixEqual( testcase.matrix, testcase.solution ) {
+			t.Errorf(
+				"Solve: returned %v, want %v",
+				testcase.matrix,
+				testcase.solution,
+			)
+		}
+	}
+}
