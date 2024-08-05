d9431260c7041d419eee07e29a367aed320f7829
Benjamin-Couey <benjamincouey36@gmail.com>
2024-08-05 08:55:45 -0600

    Improved comments for is_height_balanced

--- is_height_balanced/main.go
+++ is_height_balanced/main.go
@@ -1,3 +1,9 @@
+/*
+Package includes utility function for checking if a tree is height balanced.
+While a utility function and not the answer to a leetcode problem, it relies on
+the implementatino of max_depth_of_binary_tree and so is seperate from utils
+to avoid a circular import.
+*/
 package is_height_balanced
 
 import (
@@ -5,8 +11,11 @@ import (
 	"leetcode/utils"
 )
 
-/* TOOD: Consider moving to utils since there isn't a Leetcode problem for
-is height balanced. */
+/*
+IsHeightBalanced reports whether the tree starting at root is height balanced.
+That is, the height of the left and the right subtree of any node differ by not
+more than one.
+*/
 func IsHeightBalanced(root *utils.TreeNode) bool {
 
 	if root == nil {
